cfb6e777c9bd711072ba13cdb6271efcaf60712c
Joshua Igbokwe <joshdash.ji@gmail.com>
2023-07-18 21:34:03 +0100

    ammend

--- backend/settings.py
+++ backend/settings.py
@@ -196,10 +196,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 #MEDIA_ROOT = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
